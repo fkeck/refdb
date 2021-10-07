ee445b0c22ad2e35b2274adb4e3d1b699cf255af
fkeck <francois.keck@gmail.com>
2021-10-07 14:43:33 +0200

    Disable tests requiring external server connection

--- tests/testthat/test_import_web.R
+++ tests/testthat/test_import_web.R
@@ -1,14 +1,14 @@
 #    test <- read.csv("tests/testthat/data_toclean_bold.csv")
 
-test_that("Test download", {
-  skip_on_cran()
-  qb <- refdb_import_BOLD("Baetis melanonyx", ncbi_taxo = FALSE)
-  expect_s3_class(qb, "data.frame")
-
-  qb <- refdb_import_BOLD("Baetis melanonyx", ncbi_taxo = TRUE)
-  expect_s3_class(qb, "data.frame")
-
-  query <- "Fragilaria capucina rbcl"
-  qn <- refdb_import_NCBI(query)
-  expect_s3_class(qn, "data.frame")
-})
+# test_that("Test download", {
+#   skip_on_cran()
+#   qb <- refdb_import_BOLD("Baetis melanonyx", ncbi_taxo = FALSE)
+#   expect_s3_class(qb, "data.frame")
+#
+#   qb <- refdb_import_BOLD("Baetis melanonyx", ncbi_taxo = TRUE)
+#   expect_s3_class(qb, "data.frame")
+#
+#   query <- "Fragilaria capucina rbcl"
+#   qn <- refdb_import_NCBI(query)
+#   expect_s3_class(qn, "data.frame")
+# })
