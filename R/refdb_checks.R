b27f60619c21f099044421c48fba66f172b5b0de
fkeck <francois.keck@gmail.com>
2022-07-08 16:06:28 +0200

    Add refdb_check_seq_homogeneity

--- R/refdb_checks.R
+++ R/refdb_checks.R
@@ -203,3 +203,88 @@ refdb_check_seq_conflict <- function(x, na_omit = TRUE) {
   return(res)
 }
 
+
+
+
+
+#' Check for genetic homogeneity of taxa
+#'
+#' This function assesses the genetic similarity among
+#' sequences within each taxa.
+#' It takes user defined thresholds (one threshold per taxonomic level)
+#' to warn about sequences which are singularly different
+#' (based on median distance) from the others.
+#' Sequences in the reference database must be aligned.
+#'
+#' @param x a reference database (sequences must be aligned).
+#' @param levels a named vector of genetic similarity thresholds.
+#' Names must correspond to taxonomic levels (taxonomic fields)
+#' and values must be included in the interval [0, 1].
+#' For example to assess homogeneity at 5 percents (within species) and
+#' 10 percents (within genus): \code{levels = c(species = 0.05, genus = 0.1)}
+#' @param min_n_seq the minimum number of sequences for a
+#' taxon to be tested.
+#'
+#' @details
+#' For every tested taxonomic levels, the algorithm
+#' checks all sequences in every taxa
+#' (for which the total number of sequence is > \code{min_n_seq})
+#' In each taxon, the pairwise distance matrix among all the sequences
+#' belonging to this taxon is computed. A sequence is tagged as suspicious
+#' and returned by the function
+#' if its median genetic distance from the other sequences is higher than
+#' the threshold set by the user (\code{levels} argument).
+#'
+#'
+#' @return
+#' A dataframe reporting suspicious sequences whose median distance
+#' to other sequences of the same taxon is greater than
+#' the specified threshold. The first column "level_threshold_homogeneity"
+#' indicates the lowest taxonomic level for which the threshold has been
+#' exceeded and the second column "value_threshold_homogeneity" gives
+#' the computed median distance.
+#' @export
+#'
+refdb_check_seq_homogeneity <- function(x, levels, min_n_seq = 3) {
+
+  check_fields(x, what = c("taxonomy", "sequence"))
+
+  levels <- levels[order(match(names(levels), rev(ncbi_taxo_rank())))]
+
+  tax_cols <- attributes(x)$refdb$taxonomy
+  seq_col <- attributes(x)$refdb$sequence
+
+  # A weak test to know if sequences are aligned
+  if(!all(nchar(x[, seq_col]) == nchar(x[1, seq_col]))) {
+    stop("Sequences must be aligned.")
+  }
+
+  dat <- x[, c(tax_cols, seq_col)]
+
+  median_dist <- function(x, min_n_seq) {
+    if(length(x) < min_n_seq) {
+      return(NA)
+    }
+    seq_i_g <- bioseq::as_DNAbin(x)
+    seq_dist_i_g <- ape::dist.dna(seq_i_g, model = "raw", as.matrix = TRUE)
+    unname(apply(seq_dist_i_g, 2, stats::median))
+  }
+
+  res <- vector("list", length(levels))
+  names(res) <- names(levels)
+
+  for (i in names(levels)){
+    tax_i <- 1:which(names(tax_cols) == i)
+    dat_i <- dat[, c(tax_i, ncol(dat))]
+    dat_i <- dplyr::group_by(dat_i, dplyr::across(1:max(tax_i)))
+    dat_i <- dplyr::mutate(dat_i, med_dist = median_dist(!!rlang::sym(seq_col), min_n_seq = min_n_seq))
+    res[[i]] <- dat_i$med_dist
+  }
+
+  sel <- mapply(function(x, y) x > y, x = res, y = levels)
+  level_threshold_homogeneity <- apply(sel, 1, function(x) colnames(sel)[x][1])
+  value_threshold_homogeneity <- apply(as.data.frame(res), 1, function(x) x[x > levels][1])
+  res <- cbind(level_threshold_homogeneity, value_threshold_homogeneity, x)
+  res <- dplyr::filter(res, !is.na(level_threshold_homogeneity))
+  return(res)
+}
